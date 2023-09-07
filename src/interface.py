4dd5445d6a12e511217a522652f5721ce8a991c3
snow-fox <taijunjet@hotmail.com>
2023-09-07 14:00:09 +0100

    add flight ceiling

--- src/interface.py
+++ src/interface.py
@@ -4,7 +4,9 @@ from midware import Midware
 
 if __name__ == "__main__":
     # connect to the drone
-    drone = Midware("/dev/ttyACM0", 4, 2)
+    drone = Midware(
+        "/dev/ttyACM0", state_update_rate=4, setpoint_update_rate=2, flight_ceiling=5.0
+    )
     time.sleep(1.0)
 
     # arm, takeoff, land
