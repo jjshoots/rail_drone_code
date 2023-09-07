1fe6ee3edcee07cb661d1f4dd7adbfe81ff22ad8
snow-fox <taijunjet@hotmail.com>
2023-09-07 14:32:34 +0100

    normal script

--- src/interface.py
+++ src/interface.py
@@ -9,13 +9,8 @@ if __name__ == "__main__":
     )
     time.sleep(1.0)
 
-    _ = input("Press ENTER to arm")
-    drone.vehicle.arm(True)
-    time.sleep(10.0)
-    drone.vehicle.disarm(True)
-
     # arm, takeoff, land
-    # drone.preflight_setup()
-    # drone.takeoff()
-    # time.sleep(10.0)
-    # drone.land()
+    drone.preflight_setup()
+    drone.takeoff()
+    time.sleep(10.0)
+    drone.land()
