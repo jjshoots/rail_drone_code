1e3b8aea033ea49ee78a032d90315d5becb9e85f
snow-fox <taijunjet@hotmail.com>
2023-09-11 16:40:59 +0100

    remove terminate handlers

--- src/interface.py
+++ src/interface.py
@@ -3,24 +3,11 @@ import time
 
 from midware import Midware
 
-# handler for all the ctrl-c logic
-terminate_hooks = []
-def terminate_handler(*_):
-    print("Crtl-C invoked!")
-    for hook in terminate_hooks:
-        hook()
-    exit(1)
-
 if __name__ == "__main__":
     # connect to the drone
     drone = Midware(
         "/dev/ttyACM0", state_update_rate=4, setpoint_update_rate=2, flight_ceiling=5.0
     )
-
-    # handle terminate handlers
-    terminate_hooks.append(drone.__del__())
-    signal.signal(signal.SIGINT, terminate_handler)
-
     time.sleep(1.0)
 
     # arm, takeoff, land
