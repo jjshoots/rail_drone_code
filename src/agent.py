a3c281b8a0e3aaf8e65707fd70c70cb657d85fbe
Jet <taijunjet@hotmail.com>
2023-09-20 18:03:44 +0100

    fix wrong string

--- src/agent.py
+++ src/agent.py
@@ -162,7 +162,7 @@ class Agent:
 
     def _zmq_update_watcher(self) -> None:
         """A watchdog for the ZMQ updates. Also periodically prints out the setpoint."""
-        print(f"[go, right_drift, yaw_rate, climb_rate]: {self.setpoint}")
+        print(f"Setpoint FRDY: {self.setpoint}")
 
         # check the zmq
         stale_time = time.time() - self.last_zmq_update
